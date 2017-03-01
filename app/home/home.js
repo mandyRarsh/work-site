09bde479d78a80eb5215ba5ef3d9d1a5f0792967
Ryan Fitzpatrick <fitzpatrick.ry@gmail.com>
2017-02-28 20:59:54 -0500

    set up proj

---
+++ app/home/home.js
@@ -0,0 +1,15 @@
+'use strict';
+
+angular.module('Home', ['ngRoute'])
+
+.config(['$routeProvider', function($routeProvider) {
+  $routeProvider.when('/home', {
+    templateUrl: 'home/home.html',
+    controller: 'HomeCtrl',
+    
+  });
+}])
+
+.controller('HomeCtrl', [function() {
+
+}]);
